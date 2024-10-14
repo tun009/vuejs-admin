781ae889aad548f70cfb865ba925314d0048d561
tun009 <thanhtung04111998@gmail.com>
2024-10-14 09:09:52 +0700

    feat: fix lint, update syntax code

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -119,7 +119,6 @@ import {
   labelsValidChart
 } from '@/@types/pages/dashboard'
 import { getFieldsChanged, getSOLs } from '@/api/dashboard'
-
 const labelsValid = labelsValidChart
 const bgcolorValid = bgcolorValidChart
 const share_percentage_valid = [800, 100, 100]
