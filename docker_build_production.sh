f584b45fd63d52f064e2c5fc7594cb26412809c4
ctv_huydv <ctv_huydv@cyberspace.vn>
2024-12-13 10:34:54 +0700

    fix: duplicate rule, display table compare error

--- docker_build_production.sh
+++ docker_build_production.sh
@@ -1,4 +1,4 @@
-export IMAGE_VER=v0.0.15
+export IMAGE_VER=v0.0.18
 export IMAGE_TAG=eximbank-ui
 docker build -t $IMAGE_TAG:$IMAGE_VER .
 docker tag $IMAGE_TAG:$IMAGE_VER hub.vtcc.vn:8999/vtcc/ocr/$IMAGE_TAG:$IMAGE_VER
